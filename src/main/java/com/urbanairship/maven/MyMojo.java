706b68c37500697b6ba987bf5dc66afb3b261526
Patrick Woodworth <patrick@woodworth.org>
2013-03-06 12:36:59 -0800

    Stuff

--- src/main/java/com/urbanairship/maven/MyMojo.java
+++ src/main/java/com/urbanairship/maven/MyMojo.java
@@ -1,21 +1,5 @@
 package com.urbanairship.maven;
 
-/*
- * Copyright 2001-2005 The Apache Software Foundation.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 
@@ -30,12 +14,9 @@ import java.io.IOException;
 
 /**
  * Goal which touches a timestamp file.
- *
- * @deprecated Don't use!
  */
 @Mojo( name = "touch", defaultPhase = LifecyclePhase.PROCESS_SOURCES )
-public class MyMojo
-    extends AbstractMojo
+public class MyMojo extends AbstractMojo
 {
     /**
      * Location of the file.
